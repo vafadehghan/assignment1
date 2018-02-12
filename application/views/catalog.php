8e25cd80119aea0dd017cca4f5cf3cc9cb721a76
anthony vu <anthony_vu@live.ca>
2018-02-11 21:11:08 -0800

    updated branch

--- application/views/catalog.php
+++ application/views/catalog.php
@@ -2,62 +2,55 @@
 <link rel="stylesheet" type="text/css" href="assets/css/dragula.css">
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="/welcome">Yipper</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+      <li class="nav-item">
+        <a class="nav-link" href="/welcome">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/catalog">Catalog</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 <div class="row">
 	<div class="col-md-6">
 		<span class="player">
-		<span class="head"></span>
-		<span class="upper"></span>
-		<span class="leftHand"></span>
-		<span class="rightHand"></span>
-		<span class="leftFoot"></span>
-		<span class="rightFoot"></span>
+		  <span class="head"></span>
+		  <span class="upper"></span>
+			<span class="leftHand"></span>
+			<span class="rightHand"></span>
+			<span class="leftFoot"></span>
+			<span class="rightFoot"></span>
 
-		<img src="assets/img/man2.png">
+		  <img src="assets/img/man2.png">
 		</span>
 	</div>
 
 	<div class="col-md-6">
-		<div class="row">
-		<span class="items">
-	    <span class="hats">
-        <img src="assets/img/hat.png">
-        <img src="assets/img/hat2.png">
-        <img src="assets/img/hat3.png">
-        <img src="assets/img/hat4.png">
-        <img src="assets/img/glasses.png">
-        <img src="assets/img/glasses2.png">
-	    </span>
-			<br>
-		  <span class="tops">
-		    <img src="assets/img/sweater.png">
-		  </span>
-			<br>
-			<span class="hands">
-				<img src="assets/img/knife.png">
-			</span>
-			<br>
-			<span class="shoes">
-			<img src="assets/img/triples.png">
-			</span>
-			<br>
-		</span>
-		</div>
-		<hr>
+
 		<div class="row">
 			<span class="items">
 			{categories}
-			<span class="categoryName">
+			<span class="{categoryName}">
 			{accessories}
-			<span class="{category}">
-			<img style="width: 10%; height: width;" src="{accessoryImg}" alt="{accessoryName}">
-			</span>
+			<img src="{accessoryImg}" alt="{accessoryName}">
 			{/accessories}
 			</span>
 			<br>
 			{/categories}
 			</span>
 		</div>
+
 	</div>
+
 </div>
 </div>
 
--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,64 +1,61 @@
-var drake = dragula([document.querySelector('.hats'), document.querySelector('.head')], {
-    accepts: function (el, target, source, sibling) {
-        if (target.className === "head") {
-            if (!($(target).find('img')[0])) {
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            return true;
-        }
-
+var drake = dragula([document.querySelector('.hat'), document.querySelector('.head')], {
+  accepts: function(el, target, source, sibling) {
+    if (target.className === "head") {
+      if (!($(target).find('img')[0])) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
     }
+
+  }
 });
 
-var drake3 = dragula([document.querySelector('.upper'), document.querySelector('.tops')], {
-    accepts: function (el, target, source, sibling) {
-        if (target.className === "upper") {
-            if (!($(target).find('img')[0])) {
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            return true;
-        }
-
+var drake3 = dragula([document.querySelector('.body'), document.querySelector('.upper')], {
+  accepts: function(el, target, source, sibling) {
+    if (target.className === "upper") {
+      if (!($(target).find('img')[0])) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
     }
+
+  }
 });
 
-var drake4 = dragula([document.querySelector('.hands'), document.querySelector('.leftHand'),document.querySelector('.rightHand')], {
-    accepts: function (el, target, source, sibling) {
-		console.log("left");
-        if (target.className === "leftHand" || target.className === "rightHand") {
-					console.log("TEST");
-            if (!($(target).find('img')[0])) {
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            return true;
-        }
+var drake4 = dragula([document.querySelector('.hand'), document.querySelector('.leftHand'), document.querySelector('.rightHand')], {
+  accepts: function(el, target, source, sibling) {
+    if (target.className === "leftHand" || target.className === "rightHand") {
+      if (!($(target).find('img')[0])) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
 
-    },
-		copy:true
+  },
+  copy: true
 });
 
-var drake5 = dragula([document.querySelector('.shoes'), document.querySelector('.leftFoot'),document.querySelector('.rightFoot')], {
-    accepts: function (el, target, source, sibling) {
-        if (target.className === "leftFoot" || target.className === "rightFoot") {
-            if (!($(target).find('img')[0])) {
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            return true;
-        }
+var drake5 = dragula([document.querySelector('.foot'), document.querySelector('.leftFoot'), document.querySelector('.rightFoot')], {
+  accepts: function(el, target, source, sibling) {
+    if (target.className === "leftFoot" || target.className === "rightFoot") {
+      if (!($(target).find('img')[0])) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
 
-    },
-	copy:true
+  },
+  copy: true
 });
-
