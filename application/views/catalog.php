af002fa99839214dcf1be38593beaeda53989f6c
anthony vu <anthony_vu@live.ca>
2018-02-11 20:16:08 -0800

    Added nav bar

--- application/views/catalog.php
+++ application/views/catalog.php
@@ -2,6 +2,24 @@
 <link rel="stylesheet" type="text/css" href="assets/css/dragula.css">
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="/welcome">Yipper</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="/welcome">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/catalog">Catalog</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 <div class="row">
 	<div class="col-md-6">
 		<span class="player">
